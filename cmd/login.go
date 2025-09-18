56dc70dc6708bc6666f2d2f10c3171e1040609d9
limrunbot <contact@limrun.com>
2025-09-18 19:04:46 +0300

    helps: fix remnant help messages
    Signed-off-by: limrunbot <contact@limrun.com>

--- cmd/login.go
+++ cmd/login.go
@@ -11,13 +11,7 @@ import (
 // loginCmd represents the login command
 var loginCmd = &cobra.Command{
 	Use:   "login",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Log in to Limrun to authorize the lim CLI.",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if err := config.Login(cmd.Context()); err != nil {
 			return err
--- cmd/root.go
+++ cmd/root.go
@@ -34,13 +34,8 @@ var (
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "lim",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Create and control sandboxes for your AI agents - Android, iOS, Chrome and more!",
+	Long:  `lim allows you to interact with Limrun to get sandbox environments for your AI agent to operate.`,
 	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 		if err := initializeConfig(cmd); err != nil {
 			return err
